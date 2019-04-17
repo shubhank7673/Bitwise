e5c443e2630aa6e64b3b208cff18c7066a2a45f3
Shubhank Khare <38766127+shubhank7673@users.noreply.github.com>
2019-04-17 10:24:58 +0530

    Update urls.py

--- kodeathon/urls.py
+++ kodeathon/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('kodeathon/',views.kodeathon,name='kodeathon'),
+    path('kodeathon/',views.kodeathonf,name='kodeathon'),
 ]
