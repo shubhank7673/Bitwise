774d3df98d91a92129e498fcad83061d70ca2b74
Shubhank Khare <38766127+shubhank7673@users.noreply.github.com>
2019-04-19 00:54:13 +0530

    Update settings.py

--- Bitwise/settings.py
+++ Bitwise/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n!#87!fp6rvwkxl1m4runn$41yzb*%&sprmqva@84-%1u2_nr8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
