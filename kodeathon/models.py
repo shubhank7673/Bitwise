4be8ece858fcbbe95ff65c7c35a5f241a7845183
Shubhank Khare <38766127+shubhank7673@users.noreply.github.com>
2019-04-19 01:06:42 +0530

    Update models.py

--- kodeathon/models.py
+++ kodeathon/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class kodeathon(models.Model):
     isActive = models.BooleanField()
+    TeamEvent = models.BooleanField(default=False)
     contestName = models.CharField(max_length=20)
     sheetName = models.CharField(max_length=60)
     def __str__(self):
