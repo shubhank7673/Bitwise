12ade67c78d1e0678fc0ec28351d0f23b205d458
Shubhank Khare <38766127+shubhank7673@users.noreply.github.com>
2019-04-19 01:11:21 +0530

    Update views.py

--- team/views.py
+++ team/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def team(request):
+    return render(request,"testteam.html")
+def testteam(request):
     return render(request,"team.html")
