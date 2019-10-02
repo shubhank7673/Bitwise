37c35f58031ca57745da93ea1dc4e8a360d66da9
ThunderHarshit <46898328+ThunderHarshit@users.noreply.github.com>
2019-10-02 11:35:02 +0530

    added live kodeathon model

--- kodeathon/models.py
+++ kodeathon/models.py
@@ -7,3 +7,14 @@ class kodeathon(models.Model):
     sheetName = models.CharField(max_length=60)
     def __str__(self):
         return self.contestName
+class livekod(models.Model):
+    isActive = models.BooleanField(default=False)
+    name = models.CharField(max_length=20)
+    Q1 = models.TextField()
+    Q2 = models.TextField()
+    Q3 = models.TextField()
+    Q4 = models.TextField()
+    Q5 = models.TextField()
+    Q6 = models.TextField()
+    def __str__(self):
+        return 'Livenow'
